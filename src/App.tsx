4038707e25ac0810ccd0e3f23c5d60b7f70631ca
crisfroi <juanfroilanramos@gmail.com>
2026-06-05 06:57:50 +0100

    Add root route redirect to login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { QueryClientProvider } from '@tanstack/react-query'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { TooltipProvider } from '@/components/ui/tooltip'
 import { Toaster } from '@/components/ui/toaster'
 import { queryClient } from '@/lib/queryClient'
@@ -43,6 +43,7 @@ function App() {
         <Toaster />
         <BrowserRouter>
           <Routes>
+            <Route path="/" element={<Navigate to="/hosix/login" replace />} />
             <Route path="/hosix/login" element={<HosixLogin />} />
             <Route path="/hosix" element={<ProtectedRoute><HosixLayout /></ProtectedRoute>}>
               <Route index element={<HosixDashboard />} />
